28adb51f9723d54d59f8215293488bde21dc0527
Rengoan <91058472+Rengoan@users.noreply.github.com>
2021-10-15 12:21:15 +0200

    Update Test.java

--- ApiColecciones/src/test/Test.java
+++ ApiColecciones/src/test/Test.java
@@ -1,14 +1,15 @@
-
 package test;
 
 import generico.ClaseGenerica;
 
 public class Test {
-    
+
     public static void main(String[] args) {
-        
-        ClaseGenerica<Integer> objInt = new  ClaseGenerica(15);
-        
+
+        ClaseGenerica<Integer> objInt = new ClaseGenerica(15);
         objInt.obtenerTipo();
+
+        ClaseGenerica<String> objString = new ClaseGenerica("Santiago");
+        objString.obtenerTipo();
     }
 }
